0d8d959ed23e0d2b1328c1af8292c05e6d18a2d9
ZacWatPur <79648331+ZacWatPur@users.noreply.github.com>
2021-06-11 21:31:51 -0400

    Flask code to connect

---
+++ mars_flask.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 21:11:12 2021
+
+@author: ZPurinton
+"""
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import mars_scrape
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def index():
+
+    # Find one record of data from the mongo database
+    mars_dict = mongo.db.mars_dict.find_one()
+    # Return template and data
+    return render_template("index.html", mars=mars_dict)
+
+
+@app.route("/scrape")
+def scrape():
+  
+    mars_dict = mongo.db.Mission_To_Mars
+    mars_data = mars_scrape.scrape()
+    # Update the Mongo database using update and upsert=True
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
